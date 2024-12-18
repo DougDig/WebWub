9ea6a00d07446d330f2da3c1477d5872e9a45e19
Douglas Imler <Douglas.Imler@gmail.com>
2024-12-18 15:22:45 -0600

    project initialized

---
+++ src/main/java/TechTigers/WebWub/WebWubApplication.java
@@ -0,0 +1,13 @@
+package TechTigers.WebWub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebWubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebWubApplication.class, args);
+	}
+
+}
---
+++ src/test/java/TechTigers/WebWub/WebWubApplicationTests.java
@@ -0,0 +1,13 @@
+package TechTigers.WebWub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebWubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
